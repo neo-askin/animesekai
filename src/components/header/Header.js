827a18f4ebd3f37b1bd24b2d9a1933ae70b2e73f
neo-askin <issayamilrt@gmail.com>
2022-06-16 17:33:41 +0200

    init-commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css';
+
+
+function Header() {
+  return (
+    <div className='header'>
+       <h1>The<strong>Anime</strong>Database</h1>
+    </div>
+  )
+}
+
+export default Header
